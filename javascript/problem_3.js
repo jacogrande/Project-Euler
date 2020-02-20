22b7f280949db264b8e764bb04cdb9068feb48d5
jacogrande <prowelljackson@gmail.com>
2020-02-20 10:51:22 -0800

    solved problem 3

--- javascript/problem_3.js
+++ javascript/problem_3.js
@@ -1,6 +1,34 @@
 // What is the largest prime factor of the number 600851475143 ?
 
-//
-const largestPrime = () => {
-  
+// ––––––– solved ––––––––
+// finds the largest prime factors of the target number
+const largestPrime = (target) => {
+
+  // config
+  let prime_factors = [];
+  let finished = false;
+  let dividend = target / 2;
+
+  // loop until all prime factors are found
+  while(!finished){
+    finished = true;
+
+    // check each number to see if its a factor of
+    for( let i = 2; i < dividend; i++){
+      if (dividend % i === 0){
+        if( prime_factors.indexOf(i) === -1) prime_factors.push(i);
+        dividend = dividend / i;
+        finished = false;
+        break;
+      }
+    }
+  }
+
+  if( prime_factors.indexOf(dividend) === -1) prime_factors.push(dividend);
+
+  let largest = prime_factors.reduce( (sum, elem) => elem > sum ? elem : sum);
+  console.log(largest);
+
 }
+
+largestPrime(600851475143);
