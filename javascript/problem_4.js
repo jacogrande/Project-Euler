cba6de2b56ff22b5760ed2894833e77950fcf02f
jacogrande <prowelljackson@gmail.com>
2020-02-26 13:50:37 -0800

    Added timer to problem 4

--- javascript/problem_4.js
+++ javascript/problem_4.js
@@ -1,8 +1,50 @@
 // Find the largest palindrome made from the product of two 3-digit numbers.
-const List = require("./ADTs/linked_list.js");
-const Stack = require("./ADTs/stack.js");
-const Queue = require("./ADTs/queue.js");
+const Timer = require("./timer.js");
 
+
+// Function for determining whether or not a number is a palindrome
 const isPalindrome = (number) => {
+  let arr = Array.from(number.toString());
+
+  for(let i = 0; i < arr.length;i++){
+    if(arr[i] !== arr[arr.length-(1+i)]) {
+      return false;
+    }
+  }
+
+  return true;
 
 }
+
+// Function for finding the largest palindrome
+// digits: the digit range (in this case: 3)
+const findPalindrome = (digits) => {
+
+
+  let palindromes = [];
+  // set max and min values: max = (1 * 10^digit) - 1, min = 1*10^(digit-1)*9
+  let max = (1 * Math.pow(10, digits))-1;
+  let min = (1 * Math.pow(10, digits-1)) * 9; // this reduces the range to ensure that only the highest factors are used.
+
+  // loop through the min-max range, testing out the highest factors
+  for(let i = max; i >= min; i--){
+    for(let k = max; k >= min; k--){
+      if(isPalindrome(i*k)){
+        palindromes.push(i*k);
+      }
+    }
+  }
+  if(palindromes.length > 0){
+    return palindromes.reduce((largest, elem) => elem > largest ? elem : largest);
+  }
+
+  return null;
+
+}
+
+let timer = new Timer();
+timer.start();
+
+console.log(findPalindrome(3));
+
+console.log(`This took ${timer.end()}`);
